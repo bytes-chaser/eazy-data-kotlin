cb928b272437add161e5dad3d58aa1158534494c
AlexGoldenbergDev <47193657+alexgoldenbergdev@users.noreply.github.com>
2020-06-23 18:08:55 +0300

    Heap Sort Implementation

---
+++ src/main/kotlin/com/goldenberg/data/sorting/algorithms/HeapSort.kt
@@ -0,0 +1,21 @@
+package com.goldenberg.data.sorting.algorithms
+
+import com.goldenberg.data.enums.Order
+import com.goldenberg.data.structure.tree.heap.HeapImpl
+import com.goldenberg.data.structure.tree.heap.HeapType
+
+class HeapSort : Sorting {
+
+    override fun <T : Comparable<T>> sort(values: MutableList<T>, order: Order): MutableList<T> {
+
+        val heapType = when (order) {
+            Order.ASC -> HeapType.MIN
+            Order.DESC -> HeapType.MAX
+        }
+
+        val heap = HeapImpl<T>(heapType)
+        while (values.isNotEmpty()) heap.add(values.removeAt(0))
+        while (heap.isNotEmpty()) values.add(heap.poll())
+        return values
+    }
+}
